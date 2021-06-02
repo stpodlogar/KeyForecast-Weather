7b3888458f1b8fce6ede9e701fd0a15a17ea5552
Stephen Podlogar <stephenpodlogar@Stephens-MacBook-Pro.local>
2021-06-02 18:59:23 -0400

    Display location weather

--- weather.js
+++ weather.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const API_KEY = '8d806538f88014f91bc539358457b4a8';
-// const API_KEY = 'bbaf86cac3c0e62ef035b4a051fdf664'
 
 async function getWeather(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${API_KEY}`)
@@ -11,13 +10,34 @@ async function getWeather(city) {
     console.log(data);
 
     let location = data.name;
-    let temp = data.main.temp;
+    let temp = Math.round(data.main.temp);
     let conditions = data.weather[0].main;
 
     console.log(location, temp, conditions);
+
+    let info = 
+    `
+    <div class="location-title">
+        <h3>${location}</h3>
+        <p>Thursday, May 20th, 2021</p>
+    </div>
+    <div class="location-temp">
+        <p>${temp}&#176;F</p>
+    </div>
+    <div class="location-conditions">
+        <p>${conditions}</p>
+    </div>
+    `
+
+    // reset results to empty
+    document.querySelector('.weather-results').textContent = '';
+    // insert html for desired location
+    document.querySelector('.weather-results').insertAdjacentHTML('afterbegin', info);
 }
 
-document.querySelector('button').addEventListener('click', () => {
-    let userLocation =  document.querySelector('#location');
-    getWeather(userLocation.value);
+document.querySelector('#location').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        let userLocation =  document.querySelector('#location');
+        getWeather(userLocation.value);
+    }
 })
