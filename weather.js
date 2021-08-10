7ceb12acb672f4e65b18d7bb91a1a62aaf0941c8
Stephen Podlogar <stephenpodlogar@Stephens-MacBook-Pro.local>
2021-08-10 16:59:08 -0400

    Integrate with onecall API

--- weather.js
+++ weather.js
@@ -2,10 +2,10 @@
 
 const API_KEY = '8d806538f88014f91bc539358457b4a8';
 
-async function getWeather(city) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${API_KEY}`)
+async function getCoordinates(city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
     console.log(response.status);
-
+    
     if (response.status === 404) {
         document.querySelector('.error').textContent = "Please enter a valid city";
     }
@@ -16,26 +16,63 @@ async function getWeather(city) {
     console.log(data);
 
     // API data
+    const lat = data.coord.lat;
+    const lon = data.coord.lon;
     const location = data.name;
-    const temp = Math.round(data.main.temp);
-    const conditions = data.weather[0].description;
     const countryCode = data.sys.country; 
-    const weatherIcon = `https://weather-icons-stpodlogar.s3.us-east-2.amazonaws.com/${data.weather[0].icon}.svg`;
+
+    getWeather(lat, lon, location);
+}
+
+async function getWeather(lat, lon, location) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=imperial&appid=${API_KEY}`)
+    console.log(response.status);
+
+    const data = await response.json();
+    console.log(data);
+
+    console.log(location);
+
+    // API data
+    const temp = Math.round(data.current.temp);
+    const conditions = data.current.weather[0].description;
+    const weatherIcon = `https://weather-icons-stpodlogar.s3.us-east-2.amazonaws.com/${data.current.weather[0].icon}.svg`;
+
+    data.daily.forEach(element => {
+        console.log(element.temp.max, element.temp.min);
+        console.log(element.weather[0].main, element.weather[0].icon);
+    });
 
     let markup = 
     `
     <article class="weather-results">
-        <div class="location-title">
-            <h3><span>${location}</span><sup>${countryCode}</sup></h3>
-            <p><em>${getDate()}</em></p>
+        <div style="display: flex; justify-content: space-between">
+            <h2>Current Weather</h2>
+            <p>F or C</p>
         </div>
-        <div class="location-temp">
-            ${temp}<sup>&#176;F</sup>
-        </div>
-        <div class="location-conditions">
-            <img src=${weatherIcon}>
-            <p>${conditions.toUpperCase()}</p>
-        </div>
+        <article class="weather-data">
+            <section>
+                <div class="location-title">
+                    <h3><span>${location}</span></h3>
+                    <p><em>${getDate()}</em></p>
+                </div>
+                <div style="display: flex; align-items: center; justify-content: center">
+                    <div class="location-conditions" style="display: inline">
+                        <img src=${weatherIcon}>
+                    </div>
+                    <div class="location-temp" style="display: inline">
+                        ${temp}<sup>&#176;</sup>
+                    </div>
+                </div>
+                <p>${conditions.toUpperCase()}</p>
+            </section>
+            <section>
+
+            </section>
+        </article>
+    </article>
+    <article class="weather-results" style="margin-top: 20px">
+        <h2>Future weather</h2>
     </article>
     `
 
@@ -67,5 +104,5 @@ document.querySelector('.weather-input form').addEventListener('submit', (e) => 
     // }
     e.preventDefault();
     let userLocation =  document.querySelector('#location');
-    getWeather(userLocation.value);
+    getCoordinates(userLocation.value);
 })
